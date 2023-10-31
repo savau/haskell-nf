a25d462451434817b85ca67de0ef079bf1eb2980
Sarah Vaupel <sarah.vaupel@uniworx.de>
2023-10-31 00:38:12 +0000

    basic module structure

---
+++ haskell-nf/src/Data/NormalForm.hs
@@ -0,0 +1,9 @@
+-- SPDX-FileCopyrightText: 2023 Sarah Vaupel <sarah.vaupel@uniworx.de>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
+module Data.NormalForm
+  ( module Data.NormalForm.DNF
+  ) where
+
+import Data.NormalForm.DNF
