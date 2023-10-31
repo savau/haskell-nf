be2a1b0bfe671d50a8ebea019ce5585a9670bc44
Sarah Vaupel <sarah.vaupel@uniworx.de>
2023-10-31 17:29:34 +0000

    enhance documentation

--- haskell-nf/src/Data/NormalForm/DNF.hs
+++ haskell-nf/src/Data/NormalForm/DNF.hs
@@ -5,9 +5,9 @@
 module Data.NormalForm.DNF
   ( Literal(..)
   , DNF(..)
-  , dnfTrue, dnfFalse
+  , dnfFalse, dnfTrue
   , dnfSingleton
-  , dnfPos, dnfNeg
+  , dnfNeg, dnfPos
   , dnfAnd, dnfOr
   ) where
 
@@ -24,33 +24,48 @@ import GHC.Generics (Generic)
 
 -- | A single literal of a term may be positive or negative
 data Literal a =
-    LiteralPos { literal :: a }
-  | LiteralNeg { literal :: a }
+    LiteralNeg { literal :: a }
+  | LiteralPos { literal :: a }
   deriving (Eq, Ord, Show, Read, Data, Generic, Typeable)
   deriving anyclass (Hashable, Binary, NFData)
 
 
+-- | A disjunctive normal form is a disjunction (set) of terms, a term being a conjunction (set) of literals
 newtype DNF a = DNF { dnfTerms :: Set (Set (Literal a)) }
   deriving (Eq, Ord, Show, Read, Data, Generic, Typeable)
   deriving anyclass (Binary, Hashable, NFData)
 
-dnfTrue, dnfFalse :: DNF a
-dnfTrue  = DNF $ Set.singleton Set.empty
+
+-- | The disjunction of no terms corresponds to False
+dnfFalse :: DNF a
 dnfFalse = DNF Set.empty
 
+-- | The disjunction of a single term consisting of no literal corresponds to True
+dnfTrue :: DNF a
+dnfTrue = DNF $ Set.singleton Set.empty
+
+-- | The disjunctive normal form of a single literal
 dnfSingleton :: Literal a -> DNF a
 dnfSingleton = DNF . Set.singleton . Set.singleton
 
-dnfPos, dnfNeg :: a -> DNF a
-dnfPos = dnfSingleton . LiteralPos
+-- | A single negative literal
+dnfNeg :: a -> DNF a
 dnfNeg = dnfSingleton . LiteralNeg
 
-infixr 3 `dnfAnd`
-infixr 2 `dnfOr`
+-- | A single positive literal
+dnfPos :: a -> DNF a
+dnfPos = dnfSingleton . LiteralPos
 
-dnfAnd, dnfOr :: Ord a => DNF a -> DNF a -> DNF a
+
+infixr 3 `dnfAnd`
+-- | The intersection of two disjunctive normal forms is the cartesian product of the terms of both formulae
+dnfAnd :: Ord a => DNF a -> DNF a -> DNF a
 dnfAnd  (DNF a) (DNF b) = DNF . Set.fromList $ do
   aConj <- Set.toList a
   bConj <- Set.toList b
   return $ aConj `Set.union` bConj
+
+infixr 2 `dnfOr`
+-- | The union of two disjunctive normal forms is just the union of the formulae sets of terms
+dnfOr :: Ord a => DNF a -> DNF a -> DNF a
 dnfOr (DNF a) (DNF b) = DNF $ a <> b
