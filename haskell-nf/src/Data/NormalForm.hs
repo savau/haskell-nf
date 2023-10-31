b8a5769e74b8a81e81f8ef5ebe992e7d7aa3066d
Sarah Vaupel <sarah.vaupel@uniworx.de>
2023-10-31 02:21:46 +0000

    define Literal with instances

--- haskell-nf/src/Data/NormalForm.hs
+++
@@ -1,9 +0,0 @@
--- SPDX-FileCopyrightText: 2023 Sarah Vaupel <sarah.vaupel@uniworx.de>
---
--- SPDX-License-Identifier: AGPL-3.0-or-later
-
-module Data.NormalForm
-  ( module Data.NormalForm.DNF
-  ) where
-
-import Data.NormalForm.DNF
--- haskell-nf/src/Data/NormalForm/DNF.hs
+++ haskell-nf/src/Data/NormalForm/DNF.hs
@@ -3,7 +3,21 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 module Data.NormalForm.DNF
-  ( 
+  ( Literal(..)
   ) where
 
+import Control.DeepSeq (NFData)
 
+import Data.Binary (Binary)
+import Data.Data (Data)
+import Data.Hashable (Hashable)
+
+import GHC.Generics (Generic)
+
+
+-- | A single literal of a term may or may not be negated
+data Literal a =
+    Literal        { literal :: a }
+  | NegatedLiteral { literal :: a }
+  deriving (Eq, Ord, Show, Read, Data, Generic)
+  deriving anyclass (Hashable, Binary, NFData)
